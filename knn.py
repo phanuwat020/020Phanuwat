85ba2af712d02601076b6a1837f5f5087c080235
phanuwat020 <155074325+phanuwat020@users.noreply.github.com>
2024-11-19 16:13:06 +0700

    .

--- knn.py
+++ knn.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header('kairung')
-#st.image("./pic/kairung.jpg")
+st.header('Phanuwat')
+st.image("./img/20.jpg")
 col1, col2, col3 = st.columns(3)
 with col1:
  st.header("Versicolor")
