32ecfdd65db39803a04bb12117e208741a9e6dfd
phanuwat020 <155074325+phanuwat020@users.noreply.github.com>
2024-11-19 15:00:04 +0700

    Update home.py

--- home.py
+++ home.py
@@ -7,6 +7,6 @@ st.header("🎡🎡Website Developing using Python🎡🎡")
 st.subheader("Phanuwat Rungrueang")
 st.image('./img/020.jpg')
 
-dt=pd.read_csv('.data/iris-3.csv')
+dt=pd.read_csv('.data/iris-3.csvs')
 st.header("ข้อมูลดอกไม้")
 st.write(dt.head(10))
