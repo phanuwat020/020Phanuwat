fca327ba73ba316938d68ba698fd0713340442e4
phanuwat020 <155074325+phanuwat020@users.noreply.github.com>
2024-11-19 14:45:15 +0700

    .

--- home.py
+++ home.py
@@ -5,3 +5,8 @@ st.title("🎡🎡Website Developing using Python🎡🎡")
 st.header("🎡🎡Website Developing using Python🎡🎡")
 
 st.subheader("Phanuwat Rungrueang")
+st.image('.img/020gpg')
+
+dt=pd.read_csv('.data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
